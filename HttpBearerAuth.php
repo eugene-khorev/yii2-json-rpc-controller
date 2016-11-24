3792938296da4ae7d5dad96e5c18248fb606688e
Eugene Khorev <eugene.khorev@gmail.com>
2016-11-24 22:13:46 +0300

    Added HttpBearerAuth to handle token authentication failure

---
+++ HttpBearerAuth.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * JSON-RPC HttpBearerAuth class file
+ * @author Eugene Khorev <eugene.khorev@gmail.com>
+ */
+
+namespace jsonrpc;
+
+/**
+ * JSON RPC HttpBearerAuth is an action filter that supports the authentication method based on HTTP Bearer token.
+ */
+class HttpBearerAuth extends \yii\filters\auth\HttpBearerAuth
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function handleFailure($response)
+    {
+        $ex = new \yii\web\UnauthorizedHttpException('Your request was made with invalid credentials.');
+        
+        $response->data = [
+            'jsonrpc' => '2.0',
+            'error' => '',
+            'code' => $ex->getCode(),
+            'message' => $ex->getMessage(),
+            'data' => YII_DEBUG ? $ex->getTraceAsString() : null,
+        ];
+    }
+
+}
